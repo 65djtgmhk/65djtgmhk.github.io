5641b6db066f20596f6ad07b68c11f6855ff434e
Danil <tresk33@gmail.com>
2022-11-22 14:37:27 +0300

    asdsadad

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
+const rename = require("gulp-rename");
 
 // Static server
 gulp.task('server', function() {
